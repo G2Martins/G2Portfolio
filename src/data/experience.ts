9c82ccefc214cbf82b84047f4658ec5f500e4b6c
G2Martins <gustavogripaldi@outlook.com>
2025-06-18 00:05:11 -0300

    feat: Brasal Experience

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -4,6 +4,13 @@ export const experiences = [
       experiencetitle: 'Estagiário de TI',
       experiencecompanyName: "Procuradoria-Geral da Fazenda Nacional",
       experiencecompanyLogo: "assets/companyLogos/PGFN.png",
-      experienceduration: "(Janeiro 2024 - Present)"
+      experienceduration: "(Janeiro 2024 - Abril 2025)"
+    },
+    {
+      id: 1,
+      experiencetitle: 'Estagiário em Ciência de Dados',
+      experiencecompanyName: "Brasal Refrigerantes",
+      experiencecompanyLogo: "assets/companyLogos/Brasal.png",
+      experienceduration: "(Abril 2025 - Present)"
     }
 ];
